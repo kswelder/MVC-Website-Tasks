cd9aa15013f89a833c64a2a08e753a4abac704ff
kswelder <welderhayne2000@gmail.com>
2023-08-01 12:48:12 -0300

    Change name package

---
+++ src/main/java/kswelder/com/github/task/repositorys/TasksRepository.java
@@ -0,0 +1,11 @@
+package kswelder.com.github.task.repositorys;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import kswelder.com.github.task.models.Task;
+
+@Repository
+public interface TasksRepository extends JpaRepository<Task, Long>{
+
+}
---
+++ src/main/java/kswelder/com/github/task/services/TasksService.java
@@ -0,0 +1,8 @@
+package kswelder.com.github.task.services;
+
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class TasksService {
+}
