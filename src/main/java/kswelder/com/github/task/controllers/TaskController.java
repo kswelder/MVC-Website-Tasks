f2afd1f0e3f032b6a2fdb82017b0d47031f61631
kswelder <welderhayne2000@gmail.com>
2023-12-20 07:11:15 -0300

    Controllers

--- src/main/java/kswelder/com/github/task/controllers/TaskController.java
+++ src/main/java/kswelder/com/github/task/controllers/TaskController.java
@@ -1,4 +1,42 @@
 package kswelder.com.github.task.controllers;
 
+import kswelder.com.github.task.dto.TaskDTO;
+import kswelder.com.github.task.models.Task;
+import kswelder.com.github.task.services.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/task")
 public class TaskController {
+    @Autowired
+    private TaskService service;
+
+    @GetMapping("/list")
+    public ResponseEntity<List<TaskDTO>> getAll() {
+        return ResponseEntity.status(HttpStatus.FOUND).body(service.listTasks());
+    }
+    @GetMapping("/find/{id}")
+    public ResponseEntity<Task> getTask(@PathVariable("id") String id) {
+        return ResponseEntity.status(HttpStatus.FOUND).body(service.findTask(id));
+    }
+    @PostMapping("/save/{id}")
+    public ResponseEntity<Void> saveTask(@PathVariable("id") String id ,@RequestBody Task task) {
+        service.saveTask(id, task);
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Void> updateTask(@PathVariable("id") String id,@RequestBody Task task) {
+        service.updateTask(id, task);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable("id") String id) {
+        service.deleteTask(id);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/kswelder/com/github/task/controllers/UserController.java
+++ src/main/java/kswelder/com/github/task/controllers/UserController.java
@@ -1,4 +1,52 @@
 package kswelder.com.github.task.controllers;
 
+import kswelder.com.github.task.dto.UserDTO;
+import kswelder.com.github.task.models.User;
+import kswelder.com.github.task.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/user")
 public class UserController {
+    @Autowired
+    private UserService service;
+
+    @GetMapping("/list/all")
+    public ResponseEntity<List<UserDTO>> listAll() {
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(service.getAllUsers());
+    }
+    @GetMapping("/find/id/{id}")
+    public ResponseEntity<User> findUserId(@PathVariable("id") String id) {
+        return ResponseEntity.status(HttpStatus.FOUND).body(service.getUserById(id));
+    }
+    @GetMapping("/find/username/{username}")
+    public ResponseEntity<User> findUserUsername(@PathVariable("username") String username) {
+        return ResponseEntity.status(HttpStatus.FOUND).body(service.getUserByUsername(username));
+    }
+    @GetMapping("/find/email/{email}")
+    public ResponseEntity<User> findUserEmail(@PathVariable("email") String email) {
+        return ResponseEntity.status(HttpStatus.FOUND).body(service.getUserByEmail(email));
+    }
+    @PostMapping("/save")
+    public ResponseEntity<Void> saveUser(@RequestBody User user) {
+        service.saveUser(user);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
+    @PutMapping("/update/{id}")
+    public ResponseEntity<Void> updateUser(@PathVariable("id") String id, @RequestBody User user) {
+        service.updateUser(id, user);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteUser(@PathVariable("id") String id) {
+        service.deleteUser(id);
+        return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
+    }
 }
