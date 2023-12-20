dafc446862f210b459882e68cab348315e94cb9a
kswelder <welderhayne2000@gmail.com>
2023-12-20 07:00:56 -0300

    Pages controller

--- src/main/java/kswelder/com/github/task/controllers/PagesController.java
+++ src/main/java/kswelder/com/github/task/controllers/PagesController.java
@@ -2,6 +2,7 @@ package kswelder.com.github.task.controllers;
 
 import java.util.List;
 
+import kswelder.com.github.task.dto.TaskDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -12,46 +13,51 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import kswelder.com.github.task.models.Task;
-import kswelder.com.github.task.services.TasksService;
+import kswelder.com.github.task.services.TaskService;
 
 @Controller
-public class TasksController implements WebMvcConfigurer {
+public class PagesController implements WebMvcConfigurer {
     @Autowired
-    private TasksService tasksService;
+    private TaskService taskService;
 
     @GetMapping("/")
     public String index(final Model model) throws Exception {
-        List<Task> listTasks = tasksService.listTasks();
+        List<TaskDTO> listTasks = taskService.listTasks();
 
         model.addAttribute("tasks", listTasks);
         return "home";
     }
+
     @GetMapping("/about")
     public String about(Model model) {
         return "about";
     }
+
     @GetMapping("/task/{id}")
     public String taskPage(@PathVariable("id") String id, final Model model) {
-        model.addAttribute("task", tasksService.findTask(id));
+        model.addAttribute("task", taskService.findTask(id));
 
         return "task";
     }
+
     @PostMapping("/saveTask")
     public String saveTask(@ModelAttribute("tasks") Task task, final Model model) {
-        tasksService.saveTask(task);
+        //taskService.saveTask(task);
 
         model.addAttribute("message", "Task Created");
 
         return "retorno";
     }
+
     @GetMapping("/updateTask/{id}")
     public String updateTask(@PathVariable("id") String id,@ModelAttribute("tasks") Task task) {
-        tasksService.updateTask(id, task);
+        taskService.updateTask(id, task);
         return "redirect:/";
     }
+
     @GetMapping("/deleteTask/{id}")
     public String deleteTask(@PathVariable("id") String id, final Model model) {
-        tasksService.deleteTask(id);
+        taskService.deleteTask(id);
 
         model.addAttribute("message", "Task deleted");
 
