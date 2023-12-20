4110e521827a3efd70f7573d3f29804258533b66
kswelder <welderhayne2000@gmail.com>
2023-12-20 06:58:23 -0300

    DTOs

--- src/main/java/kswelder/com/github/task/dto/TaskDTO.java
+++ src/main/java/kswelder/com/github/task/dto/TaskDTO.java
@@ -1,4 +1,31 @@
 package kswelder.com.github.task.dto;
 
+import kswelder.com.github.task.models.Task;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TaskDTO {
+    private String id;
+    private String name;
+    private LocalDateTime task_start;
+    private LocalDateTime task_end;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public TaskDTO(Task task) {
+        this.id = task.getId();
+        this.name = task.getName();
+        this.task_start = task.getTask_start();
+        this.task_end = task.getTask_end();
+        this.createdAt = task.getCreatedAt();
+        this.updatedAt = task.getUpdatedAt();
+    }
 }
--- src/main/java/kswelder/com/github/task/dto/UserDTO.java
+++ src/main/java/kswelder/com/github/task/dto/UserDTO.java
@@ -1,4 +1,29 @@
 package kswelder.com.github.task.dto;
 
+import kswelder.com.github.task.models.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
+    private String id;
+    private String username;
+    private String email;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.username = user.getUsername();
+        this.email = user.getEmail();
+        this.createdAt = user.getCreatedAt();
+        this.updatedAt = user.getUpdatedAt();
+    }
 }
