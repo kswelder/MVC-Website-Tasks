d7119c1ebd6a703475493be3bcfd89523e0a885d
kswelder <welderhayne2000@gmail.com>
2023-12-20 06:59:49 -0300

    Model relation task to user

--- src/main/java/kswelder/com/github/task/models/Task.java
+++ src/main/java/kswelder/com/github/task/models/Task.java
@@ -2,12 +2,13 @@ package kswelder.com.github.task.models;
 
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Getter
@@ -16,10 +17,21 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Task {
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private String id;
+    @Column(nullable = false)
+    @NotBlank(message = "Name task is mandatory")
     private String name;
+    @Column(nullable = false)
+    @NotBlank(message = "Start task is mandatory")
     private LocalDateTime task_start;
+    @Column(nullable = false)
+    @NotBlank(message = "End task is mandatory")
     private LocalDateTime task_end;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
 }
--- src/main/java/kswelder/com/github/task/models/User.java
+++ src/main/java/kswelder/com/github/task/models/User.java
@@ -1,4 +1,38 @@
 package kswelder.com.github.task.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "task_users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column(unique = true, nullable = false)
+    @NotBlank(message = "Name is mandatory")
+    private String username;
+    @Column(unique = true, nullable = false)
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+    @Column(nullable = false)
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "user")
+    private List<Task> tasks;
 }
