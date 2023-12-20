5397b74b5d3d38ec60d30f35a03e2c1fcf7c68ce
kswelder <welderhayne2000@gmail.com>
2023-12-20 07:11:42 -0300

    Repositorys

--- src/main/java/kswelder/com/github/task/repositorys/TaskRepository.java
+++ src/main/java/kswelder/com/github/task/repositorys/TaskRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import kswelder.com.github.task.models.Task;
 
 @Repository
-public interface TasksRepository extends JpaRepository<Task, String>{
+public interface TaskRepository extends JpaRepository<Task, String>{
     Task findByName(String name);
 }
